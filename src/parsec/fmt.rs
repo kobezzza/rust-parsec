864a3b9a8880cc5044cfd7759a83a9fe900c2790
kobezzza <kobezzza@gmail.com>
2026-02-24 15:30:57 +0300

    feat!: Переработал комбинатор fmt, чтобы он мог возвращать ошибку

--- src/parsec/fmt.rs
+++ src/parsec/fmt.rs
@@ -1,10 +1,9 @@
 use super::*;
-use crate::parsec::Parser;
 
 pub fn fmt<P, F, O>(p: P, fmt: F) -> Fmt<P, F>
 where
     P: Parser,
-    F: Fn(P::Output) -> O,
+    F: Fn(P::Output, ParserIterator) -> ParserResult<O>,
     O: Debug,
 {
     Fmt { p, fmt }
@@ -19,13 +18,16 @@ pub struct Fmt<P, F> {
 impl<P, F, O> Parser for Fmt<P, F>
 where
     P: Parser,
-    F: Fn(P::Output) -> O,
+    F: Fn(P::Output, ParserIterator) -> ParserResult<O>,
     O: Debug,
 {
     type Output = O;
 
     fn parse<'a>(&self, i: ParserIterator<'a>) -> ParserResult<'a, Self::Output> {
-        let (output, remaining) = self.p.parse(i)?;
-        Ok(((self.fmt)(output), remaining))
+        let (output, remaining) = self.p.parse(i.clone())?;
+
+        let (output, remaining) = (self.fmt)(output, remaining)?;
+
+        Ok((output, remaining))
     }
 }
